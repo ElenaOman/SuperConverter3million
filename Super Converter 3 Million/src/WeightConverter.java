5f67354236ea730e5851964c4f4e9091b5892ad8
robert.kulbacki <robert.kulbacki@mu-c1-03.mullen.pvt.k12.co.us>
2018-11-14 15:00:37 -0700

    NEW CLASS

---
+++ Super Converter 3 Million/src/WeightConverter.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class WeightConverter
+	{
+		public static void weightConverter()
+			{
+				String [ ] units = new String [3];
+				units [0] = "Pounds";
+				units [1] = "Ounces";
+				units [2] = "Grams";
+				
+				Scanner userInput = new Scanner(System.in);
+				System.out.println("Please choose which unit you would like to convert from.");
+				for (int i=0; i<units.length; i++)
+					{
+						System.out.println((i+1)+ " " + units[i]);
+					}
+				int cunit = userInput.nextInt();
+				
+				System.out.println("Please choose which unit you would like to convert to.");
+				for (int i=0; i<units.length; i++)
+					{
+						System.out.println((i+1)+ " " + units[i]);
+					}
+				
+				int newUnit= userInput.nextInt();
+				
+				System.out.println("How many " + units[cunit-1] + " would you like to convert to " + units[newUnit-1] + "?");
+				int num= userInput.nextInt();
+				
+				
+				
+				
+				
+			}
+	}

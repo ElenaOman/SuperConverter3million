5735344d95d1fdea9a8b9ba8299e49ca41ee69be
dunham.perry <dunham.perry@MU-C1-05.mullen.pvt.k12.co.us>
2018-11-14 14:52:51 -0700

    Started time zones

---
+++ Super Converter 3 Million/src/timeZoneConverter.java
@@ -0,0 +1,44 @@
+
+public class timeZoneConverter
+	{
+
+		public static void main(String[] args)
+			{
+			System.out.println("What time zone would you like to convert FROM?");
+			System.out.println("What time zone would you like to convert TO?");
+			}
+		
+		private static void timeZoneList(){
+			System.out.println("1) GMT (Greenwich Mean Time)");
+			System.out.println("2) CET (Central European Time)"); //+1
+			System.out.println("3) CAT (Central African Time)"); //+2
+			System.out.println("4) AST (Arabia Standard Time)"); //+3
+			System.out.println("5) IRST (Iran Standard Time)"); //+3:30
+			System.out.println("6) GST (Gulf Standard Time)"); //+4
+			System.out.println("7) AFT (Afghanistan Time)"); //+4:30
+			System.out.println("8) PKT (Pakistan Standard Time)"); //+5
+			System.out.println("9) IST (India Standard Time)"); //+5:30
+			System.out.println("10) NPT (Nepal Time)"); //+5:45
+			System.out.println("11) BIOT (British Indian Ocean Time)"); //+6
+			System.out.println("12) MMT (Myanmar Standard Time)"); //+6:30
+			System.out.println("13) ICT (Indochina Time)"); //+7
+			System.out.println("14) CST (China Standard Time)"); //+8
+			System.out.println("15) ACWST (Australian Central Western Standard Time)"); //+8:45
+			System.out.println("16) JST (Japan Standard Time)"); //+9
+			System.out.println("17) ACST (Australian Central Standard Time)"); //+9:30
+			System.out.println("18) AEST (Australian Eastern Standard Time)"); //+10
+			System.out.println("19) VUT (Vanuatu Time)"); //+11
+			System.out.println("20) NZST (New Zealand Standard Time)"); //+12
+			System.out.println("21) CHAST (Chatham Standard Time)"); //+12:45
+			System.out.println("22) TOT (Tonga Time)"); //+13
+			System.out.println("23)  ()");
+			System.out.println("24)  ()");
+			System.out.println("25)  ()");
+			System.out.println("26)  ()");
+			System.out.println("27)  ()");
+			System.out.println("28)  ()");
+			System.out.println("29)  ()");
+			
+		}
+
+	}

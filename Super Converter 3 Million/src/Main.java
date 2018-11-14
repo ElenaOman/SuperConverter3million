322262c90de243260eabf23b7012b08c98c11538
dunham.perry <dunham.perry@MU-C1-05.mullen.pvt.k12.co.us>
2018-11-14 14:20:45 -0700

    fixed

--- Super Converter 3 Million/src/Main.java
+++ Super Converter 3 Million/src/Main.java
@@ -4,17 +4,6 @@ public class Main
 		public static void main(String[] args)
 			{
 				//okay, this is epic
-				//chief called, he said this was it	
-
-			}
-
-	}
-public class Main
-	{
-
-		public static void main(String[] args)
-			{
-				//okay, this is epic
 				
 				helpDan();
 				

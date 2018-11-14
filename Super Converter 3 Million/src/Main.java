807fe554eddb2e4cad593f3efd6790267faac1ca
daniel.dominguez <daniel.dominguez@MU-C1-02.mullen.pvt.k12.co.us>
2018-11-14 12:26:34 -0700

    lmao help me please

--- Super Converter 3 Million/src/Main.java
+++ Super Converter 3 Million/src/Main.java
@@ -5,8 +5,16 @@ public class Main
 		public static void main(String[] args)
 			{
 				//okay, this is epic
-				//HELP
+				
+				helpDan();
 
 			}
+
+		private static void helpDan()
+			{
+				System.out.println("HELP ME PLEASE");
+				
+			}
+		
 
 	}

6d48962b0dd2a2174a9405cdf8dfd9d0573f822e
robert.kulbacki <robert.kulbacki@mu-c1-03.mullen.pvt.k12.co.us>
2018-11-15 14:37:15 -0700

    WEight converter complete;

--- Super Converter 3 Million/src/WeightConverter.java
+++ Super Converter 3 Million/src/WeightConverter.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class WeightConverter
 	{
+		static boolean run = true;
+		static double num;
+		static double converted;
 		public static void weightConverter()
 			{
 				String [ ] units = new String [3];
@@ -10,6 +13,10 @@ public class WeightConverter
 				units [2] = "Grams";
 				
 				Scanner userInput = new Scanner(System.in);
+			while (run==true)
+			{
+					
+				
 				System.out.println("Please choose which unit you would like to convert from.");
 				for (int i=0; i<units.length; i++)
 					{
@@ -25,8 +32,63 @@ public class WeightConverter
 				
 				int newUnit= userInput.nextInt();
 				
-				System.out.println("How many " + units[cunit-1] + " would you like to convert to " + units[newUnit-1] + "?");
-				int num= userInput.nextInt();
+				if (cunit==newUnit)
+					{
+						System.out.println("This is the same unit. " + num + " " + newUnit);
+					}
+				
+				else
+					{
+						System.out.println("How many " + units[cunit-1] + " would you like to convert to " + units[newUnit-1] + "?");
+						num= userInput.nextDouble();
+					}
+				
+				if (cunit==1 && newUnit==2)
+					{
+						converted = num*16;
+						System.out.println("There are " + converted + " " + units[newUnit-1] + " in " + num + " "+ units[cunit-1]+".");
+					}
+				else if (cunit==1 && newUnit==3)
+					{
+						converted = num*453;
+						System.out.println("There are " + converted + " " + units[newUnit-1] + " in " + num + " "+ units[cunit-1]+".");
+					}
+				else if (cunit==2 && newUnit==1)
+					{
+						converted = num*(.0625);
+						System.out.println("There are " + converted + " " + units[newUnit-1] + " in " + num + " "+ units[cunit-1]+".");
+					}
+				else if (cunit==2 && newUnit==3)
+					{
+						converted = num*(28.3945);
+						System.out.println("There are " + converted + " " + units[newUnit-1] + " in " + num + " "+ units[cunit-1]+".");
+					}
+				else if (cunit==3 && newUnit==1)
+					{
+						converted = num*(0.00220462);
+						System.out.println("There are " + converted + " " + units[newUnit-1] + " in " + num + " "+ units[cunit-1]+".");
+					}
+				else if (cunit==3 && newUnit==2)
+					{
+						converted = num*(0.035274);
+						System.out.println("There are " + converted + " " + units[newUnit-1] + " in " + num + " "+ units[cunit-1]+".");
+					}
+				
+				System.out.println("Would you like to make another conversion?");
+				String convertAgain = userInput.nextLine();
+
+				if (convertAgain.equals("true"))
+					{
+						run=true;
+					}
+				else
+					{
+						run=false;
+					}
+				
+				}
+				
+				System.out.println("THANKS FOR CONVERTING!!!!!!!!!!!!!!");	
 				
 				
 				

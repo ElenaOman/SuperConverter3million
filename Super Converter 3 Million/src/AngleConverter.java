1e2498687a7e7bd22663c255322a597bebff1207
daniel.dominguez <daniel.dominguez@MU-C1-02.mullen.pvt.k12.co.us>
2018-11-15 14:28:21 -0700

    Angle converter

---
+++ Super Converter 3 Million/src/AngleConverter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class AngleConverter
+	{
+		public static void main(String[] args)
+		{
+			Scanner userInput = new Scanner(System.in);
+			
+			System.out.println("Would you like to convert 1)Degrees to radians 2)Radians to degrees ?");
+			
+			int choice = userInput.nextInt();
+			
+			if(choice == 1)
+				{
+					System.out.println("Please input your angle.");
+					double degrees = userInput.nextDouble();
+					
+					System.out.println(degrees * .0174533);
+				}
+			else if(choice == 2)
+				{
+					System.out.println("Please input your angle.");
+					double radians = userInput.nextDouble();
+					
+					System.out.println(radians * 57.2958);
+					
+					//TEST
+					
+				}
+
+		}
+	}
+

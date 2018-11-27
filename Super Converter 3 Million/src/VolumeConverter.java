750a51971a6cecf2a6fcabe1120b5c5762fea228
Elena.Oman <Elena.Oman@MU-C1-21.mullen.pvt.k12.co.us>
2018-11-27 08:50:38 -0700

    start liter conversions

--- Super Converter 3 Million/src/VolumeConverter.java
+++ Super Converter 3 Million/src/VolumeConverter.java
@@ -6,139 +6,79 @@ public class VolumeConverter
 		public static void main(String[] args)
 			{
 				
-				String [] iu = new String[6];
-				iu[0] = "Liters";
-				iu[1] = "Milliliters";
-				iu[2] = "Ounces";
-				iu[3] = "Pints";
-				iu[4] = "Quarts";
-				iu[5] = "Gallons";
+				String [] unit = new String[6];
+				unit[0] = "Liters";
+				unit[1] = "Milliliters";
+				unit[2] = "Ounces";
+				unit[3] = "Pints";
+				unit[4] = "Quarts";
+				unit[5] = "Gallons";
 				
+				//User Input
 				Scanner userInput = new Scanner(System.in);
 
 				//Initial Unit
 				System.out.println("Which unit are you converting from?");
-				for(int i = 0; i < iu.length; i++)
+				for(int i = 0; i < unit.length; i++)
 					{
-						System.out.println((i + 1) + " " + iu[i]);
+						System.out.println((i + 1) + " " + unit[i]);
 					}
-				double initialUnit = userInput.nextDouble();
+				int initialUnit = userInput.nextInt();
 				
 				//Final Unit
 				System.out.println("Which unit are you converting to?");
-				for(int i = 0; i < iu.length; i++)
+				for(int i = 0; i < unit.length; i++)
 					{
-						System.out.println((i + 1) + " " + iu[i]);
+						System.out.println((i + 1) + " " + unit[i]);
 					}
-				double finalUnit = userInput.nextDouble();
+				int finalUnit = userInput.nextInt();
 				
 				//Conversions
 				
 				if(initialUnit == finalUnit)
 					{
-						System.out.println("There is no conversion, they are the same unit!");
+						System.out.println("How many " + unit[initialUnit - 1] + " would you like to convert to " + unit[finalUnit - 1] + "?" );
+						double num = userInput.nextDouble();
+						System.out.println("Well, there is no need for conversion, they are the same unit!");
+					}
+				else
+					{
+						System.out.println("How many " + unit[initialUnit - 1] + " would you like to convert to " + unit[finalUnit - 1] + "?" );
 					}
 				
-				else if(initialUnit == 1 && finalUnit == 2)
+				double num = userInput.nextDouble();
+				
+				//Liters
+				if(initialUnit == 1 && finalUnit == 2)
 					{
-						System.out.println("How many liters would you like to convert to milliliters?");
+						double convert = num*1000;
+						System.out.println("There are " + convert + " milliliters in " + num + " liters.");
 					}
 				
 				else if(initialUnit == 1 && finalUnit == 3)
 					{
-						System.out.println("How many liters would you like to convert to milliliters?");
+						double convert = num*33.184;
+						System.out.println("There are " + convert + " ounces in " + num + " liters.");
+						
 					}
 				
 				else if(initialUnit == 1 && finalUnit == 4)
 					{
-						System.out.println("How many liters would you like to convert to milliliters?");
+						double convert = num*2.113;
+						System.out.println("There are " + convert + " pints in " + num + " liters.");
 					}
 				
 				else if(initialUnit == 1 && finalUnit == 5)
 					{
-						System.out.println("How many liters would you like to convert to milliliters?");
+						double convert = num*1.057;
+						System.out.println("There are " + convert + " quarts in " + num + " liters.");
 					}
 				
-				else if(initialUnit == 2 && finalUnit == 1)
+				else if(initialUnit == 1 && finalUnit == 6)
 					{
-						System.out.println("How many liters would you like to convert to milliliters?");
+						double convert = num*0.264;
+						System.out.println("There are " + convert + " gallons in " + num + " liters.");
 					}
-				
-				else if(initialUnit == 2 && finalUnit == 3)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-				
-				else if(initialUnit == 2 && finalUnit == 4)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-				
-				else if(initialUnit == 2 && finalUnit == 5)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-				else if(initialUnit == 3 && finalUnit == 1)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-				else if(initialUnit == 3 && finalUnit == 2)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 3 && finalUnit == 4)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 3 && finalUnit == 5)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-				else if(initialUnit == 4 && finalUnit == 1)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-				else if(initialUnit == 4 && finalUnit == 2)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 4 && finalUnit == 3)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 4 && finalUnit == 5)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 5 && finalUnit == 1)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 5 && finalUnit == 2)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 5 && finalUnit == 3)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-				else if(initialUnit == 5 && finalUnit == 4)
-					{
-						System.out.println("How many liters would you like to convert to milliliters?");
-					}
-
-
-
-
-
 
 			}
 

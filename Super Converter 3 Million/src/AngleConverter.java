f0b8564891359753fb9cefc8fa21969aa702ee6f
daniel.dominguez <daniel.dominguez@MU-C1-02.mullen.pvt.k12.co.us>
2018-11-29 08:32:20 -0700

    Updated and clean version of speed and angles converter

--- Super Converter 3 Million/src/AngleConverter.java
+++ Super Converter 3 Million/src/AngleConverter.java
@@ -2,28 +2,64 @@ import java.util.Scanner;
 
 public class AngleConverter
 	{
+		static double pi = Math.PI;
+		static int choice;
+		static double degrees;
+		static double radians;
+
+		
+		public static void piToDegrees()
+		{
+			
+		}
+		
+		
+		
 		public static void convertAngles()
 		{
 			Scanner userInput = new Scanner(System.in);
 			
-			System.out.println("Would you like to convert 1)Degrees to radians 2)Radians to degrees ?");
+			System.out.println("Would you like to convert?");
+			System.out.println("1) Degrees to Radians");
+			System.out.println("2) Radians to Degrees");
 			
-			int choice = userInput.nextInt();
+			
+			choice = userInput.nextInt();
 			
 			if(choice == 1)
 				{
 					System.out.println("Please input your angle.");
-					double degrees = userInput.nextDouble();
-					
-					System.out.println(degrees + " degrees is " +degrees * .0174533 + " radians.");
+					degrees = userInput.nextDouble();
+					if(degrees == 45)
+						{
+							System.out.println(degrees + " degrees is pi/4 radians.");
+						}
+					else if(degrees == 90)
+						{
+							System.out.println(degrees + " degrees is pi/2 radians.");
+						}
+					else if(degrees == 30)
+						{
+							System.out.println(degrees + " degrees is pi/6 radians.");
+						}
+					else if(degrees == 60)
+						{
+							System.out.println(degrees + " degrees is pi/3 radians.");
+						}
+					else
+						{
+							System.out.println(degrees + " degrees is " + degrees * .0174533 + " radians.");
+						}
 				}
 			else if(choice == 2)
 				{
+					
+					
 					System.out.println("Please input your angle.");
-					Double radians = userInput.nextDouble();
+					radians = userInput.nextDouble();
 					
 					System.out.println(radians + " radians is " + radians * 57.2958 + " degrees.");
-					//done
+					
 					
 
 					
